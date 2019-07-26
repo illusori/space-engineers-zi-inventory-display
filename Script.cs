602e37d72b57009e8a7fec03c72d967c8666e0a9
Sam Graham <git@illusori.co.uk>
2019-07-26 10:50:03 +0100

    Add basic cargo space summary.

---
+++ Script.cs
@@ -0,0 +1,264 @@
+﻿string _script_name = "Zephyr Industries Inventory Display";
+string _script_version = "1.2.0";
+
+string _script_title = null;
+string _script_title_nl = null;
+
+const int INV_SAMPLES  = 10;
+const int LOAD_SAMPLES = 10;
+const int TIME_SAMPLES = 10;
+
+const int CYCLES_TOP  = 0;
+const int CYCLES_INV  = 1;
+const int SIZE_CYCLES = 2;
+
+const int PANELS_DEBUG = 0;
+const int PANELS_INV   = 1;
+const int SIZE_PANELS  = 2;
+
+List<string> _panel_tags = new List<string>(SIZE_PANELS) { "@DebugDisplay", "@InventoryDisplay" };
+
+/* Genuine global state */
+List<int> _cycles = new List<int>(SIZE_CYCLES);
+List<List<IMyTextPanel>> _panels = new List<List<IMyTextPanel>>(SIZE_PANELS);
+List<IMyTerminalBlock> _inventory_blocks = new List<IMyTerminalBlock>();
+List<IMyCargoContainer> _cargo_blocks = new List<IMyCargoContainer>();
+
+List<Dictionary<string, MyFixedPoint>> _item_counts = new List<Dictionary<string, MyFixedPoint>>(INV_SAMPLES);
+List<long> _load = new List<long>();
+List<long> _time = new List<long>();
+
+/* Reused single-run state objects, only global to avoid realloc/gc-thrashing */
+List<MyInventoryItem> items = new List<MyInventoryItem>();
+IMyInventory inv = null;
+MyFixedPoint existing = (MyFixedPoint)0.0;
+
+public Program() {
+    _script_title = $"{_script_name} v{_script_version}";
+    _script_title_nl = $"{_script_name} v{_script_version}\n";
+
+    for (int i = 0; i < SIZE_CYCLES; i++) {
+        _cycles.Add(0);
+    }
+    for (int i = 0; i < SIZE_PANELS; i++) {
+        _panels.Add(new List<IMyTextPanel>());
+    }
+    for (int i = 0; i < LOAD_SAMPLES; i++) {
+        _load.Add(0L);
+    }
+    for (int i = 0; i < TIME_SAMPLES; i++) {
+        _time.Add(0L);
+    }
+    for (int i = 0; i < INV_SAMPLES; i++) {
+        _item_counts.Add(new Dictionary<string, MyFixedPoint>());
+    }
+
+    FindPanels();
+    FindInventoryBlocks();
+    FindCargoBlocks();
+
+    if (!Me.CustomName.Contains(_script_name)) {
+        // Update our block to include our script name
+        Me.CustomName = $"{Me.CustomName} - {_script_name}";
+    }
+    Log(_script_title);
+
+    Runtime.UpdateFrequency |= UpdateFrequency.Update100;
+}
+
+public void Save() {
+}
+
+public void Main(string argument, UpdateType updateSource) {
+    try {
+        if ((updateSource & UpdateType.Update100) != 0) {
+	    DateTime start_time = DateTime.Now;
+
+	    _cycles[CYCLES_TOP]++;
+
+            ClearPanels(PANELS_DEBUG);
+
+            Log(_script_title_nl);
+
+            if ((_cycles[CYCLES_TOP] % 30) == 0) {
+                FindPanels();
+                FindInventoryBlocks();
+                FindCargoBlocks();
+            }
+
+            CheckInventory();
+
+	    _load[_cycles[CYCLES_TOP] % LOAD_SAMPLES] = Runtime.CurrentInstructionCount;
+	    _time[_cycles[CYCLES_TOP] % TIME_SAMPLES] = (DateTime.Now - start_time).Ticks;
+
+	    long load_avg = _load.Sum() / LOAD_SAMPLES;
+	    long time_avg = (_time.Sum() * 1000L) / (TIME_SAMPLES * TimeSpan.TicksPerMillisecond);
+	    Log($"Load avg {load_avg}/{Runtime.MaxInstructionCount} in {time_avg}us");
+
+            for (int i = 0; i < 5; i++) {
+                long load = _load[(LOAD_SAMPLES + _cycles[CYCLES_TOP] - i) % LOAD_SAMPLES];
+                long time = (_time[(TIME_SAMPLES + _cycles[CYCLES_TOP] - i) % TIME_SAMPLES] * 1000L) / TimeSpan.TicksPerMillisecond;
+                Log($"  [T-{i}] Load {load} in {time}us");
+            }
+        }
+    } catch (Exception e) {
+        Log("An exception occurred during script execution.");
+        Log($"Exception: {e}\n---");
+        throw;
+    }
+}
+
+public void CheckInventory() {
+    _cycles[CYCLES_INV]++;
+
+    //Log("Boop!");
+    int last = (_cycles[CYCLES_INV] + 1) % INV_SAMPLES,
+        current = _cycles[CYCLES_INV] % INV_SAMPLES;
+    Log($"[Inv run {_cycles[CYCLES_INV]}] Offsets: last {last}, current {current}");
+
+    _item_counts[current].Clear();
+    int num_invs = 0;
+    foreach (IMyTerminalBlock inventory_block in _inventory_blocks) {
+        if (inventory_block == null) {
+            //Log("Block is null.");
+            continue;
+        }
+        //Log("GetInv");
+        for (int i = 0, sz = inventory_block.InventoryCount; i < sz; i++) {
+            num_invs++;
+            inv = inventory_block.GetInventory(i);
+            if (inv == null) {
+                //Log("Block has null inventory.");
+            }
+            //Log("GetItems");
+            items.Clear();
+            inv.GetItems(items);
+            //Log("GotItems");
+            if (items == null) {
+                //Log("No items found.");
+                continue;
+            }
+            //Log("item in items");
+            string item_name = null;
+            foreach (MyInventoryItem item in items) {
+                if (item == null) {
+                    //Log("Found null item");
+                    continue;
+                }
+                //Log($"Found {item.Type.TypeId} {item.Type.SubtypeId} {item.Amount}");
+                //Log($"Found {item.Type.SubtypeId} {item.Amount}");
+                item_name = GetItemName(item.Type);
+                existing = (MyFixedPoint)0.0;
+                _item_counts[current].TryGetValue(item_name, out existing);
+                _item_counts[current][item_name] = MyFixedPoint.AddSafe(existing, item.Amount);
+            }
+        }
+    }
+    Log($"  {num_invs} inventories in {_inventory_blocks.Count} blocks.");
+
+    MyFixedPoint used_mass = (MyFixedPoint)0.0;
+    MyFixedPoint used_volume = (MyFixedPoint)0.0;
+    MyFixedPoint max_volume = (MyFixedPoint)0.0;
+    num_invs = 0;
+    foreach (IMyCargoContainer cargo_block in _cargo_blocks) {
+        if (cargo_block == null) {
+            //Log("Block is null.");
+            continue;
+        }
+        for (int i = 0, sz = cargo_block.InventoryCount; i < sz; i++) {
+            num_invs++;
+            inv = cargo_block.GetInventory(i);
+            if (inv == null) {
+                //Log("Block has null inventory.");
+            }
+            used_mass   = MyFixedPoint.AddSafe(used_mass, inv.CurrentMass);
+            used_volume = MyFixedPoint.AddSafe(used_volume, inv.CurrentVolume);
+            max_volume  = MyFixedPoint.AddSafe(max_volume, inv.MaxVolume);
+        }
+    }
+    Log($"  {num_invs} inventories in {_cargo_blocks.Count} cargoes.");
+
+    MyFixedPoint old, value;
+    int delta;
+    //ClearPanels(PANELS_INV);
+    WritePanels(PANELS_INV, _script_title_nl + "\n", false);
+
+    MyFixedPoint free_volume = MyFixedPoint.AddSafe(max_volume, -used_volume);
+    WritePanels(PANELS_INV, $"{(int)used_mass}kg {(int)used_volume}/{(int)max_volume}m3 {(int)free_volume}m3 free.\n\n", true);
+    foreach (KeyValuePair<string, MyFixedPoint> kvp in _item_counts[current]) {
+        value = kvp.Value;
+        old = (MyFixedPoint)0.0;
+        _item_counts[last].TryGetValue(kvp.Key, out old);
+        delta = (int)MyFixedPoint.AddSafe(value, old == null ? -value : -old) / INV_SAMPLES;
+        if (delta != 0) {
+            WritePanels(PANELS_INV, $"{(int)value,8} {kvp.Key} [{delta,0:+#;-#;0}]\n", true);
+        } else {
+            WritePanels(PANELS_INV, $"{(int)value,8} {kvp.Key}\n", true);
+        }
+    }
+}
+
+public string GetItemName(MyItemType item_type) {
+    if (item_type.TypeId == "MyObjectBuilder_Ingot") {
+        if (item_type.SubtypeId == "Stone")
+            return "Gravel";
+        return $"{item_type.SubtypeId} Ingot";
+    }
+    if (item_type.TypeId == "MyObjectBuilder_Ore") {
+        if (item_type.SubtypeId == "Stone" || item_type.SubtypeId == "Ice")
+            return item_type.SubtypeId;
+        return $"{item_type.SubtypeId} Ore";
+    }
+    if (item_type.TypeId == "MyObjectBuilder_Component") {
+        return item_type.SubtypeId;
+    }
+    if (item_type.TypeId == "MyObjectBuilder_PhysicalGunObject") {
+        return item_type.SubtypeId;
+    }
+    if (item_type.TypeId == "MyObjectBuilder_GasContainerObject") {
+        return item_type.SubtypeId;
+    }
+    return $"{item_type.TypeId} {item_type.SubtypeId}";
+}
+
+public void FindPanels() {
+    for (int i = 0; i < SIZE_PANELS; i++) {
+        _panels[i].Clear();
+        GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(_panels[i], block => block.CustomName.Contains(_panel_tags[i]));
+    }
+}
+
+public void FindInventoryBlocks() {
+    _inventory_blocks.Clear();
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(_inventory_blocks, block => block.HasInventory);
+    //Log($"Found {_inventory_blocks.Count} inventory blocks.");
+}
+
+public void FindCargoBlocks() {
+    _cargo_blocks.Clear();
+    GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(_cargo_blocks, block => block.HasInventory);
+    //Log($"Found {_cargo_blocks.Count} cargo blocks.");
+}
+
+public void ClearAllPanels() {
+    for (int i = 0; i < SIZE_PANELS; i++) {
+        WritePanels(i, "", false);
+    }
+}
+
+public void ClearPanels(int kind) {
+    WritePanels(kind, "", false);
+}
+
+public void WritePanels(int kind, string s, bool append) {
+    foreach (IMyTextPanel panel in _panels[kind]) {
+        if (panel != null) {
+            panel.WriteText(s, append);
+        }
+    }
+}
+
+public void Log(string s) {
+    WritePanels(PANELS_DEBUG, s + "\n", true);
+    Echo(s);
+}
