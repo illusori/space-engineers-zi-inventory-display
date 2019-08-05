bdb93d699cbcd22a149b2aa5cd45579375f1ac4a
Sam Graham <git@illusori.co.uk>
2019-08-05 03:22:47 +0100

    DrawBuffer support and Charts.

--- Script.cs
+++ Script.cs
@@ -1,13 +1,13 @@
 ﻿string _script_name = "Zephyr Industries Inventory Display";
-string _script_version = "1.2.5";
+string _script_version = "1.3.0";
 
 string _script_title = null;
 string _script_title_nl = null;
 
-const int INV_HISTORY   = 30;
-const int LOAD_HISTORY  = 30;
-const int TIME_HISTORY  = 30;
-const int CARGO_HISTORY = 30;
+const int INV_HISTORY   = 100;
+const int LOAD_HISTORY  = 100;
+const int TIME_HISTORY  = 100;
+const int CARGO_HISTORY = 100;
 
 const int INV_SAMPLES   = 10;
 const int LOAD_SAMPLES  = 10;
@@ -19,39 +19,18 @@ const int CYCLES_INV   = 1;
 const int CYCLES_CARGO = 2;
 const int SIZE_CYCLES  = 3;
 
-const int PANELS_DEBUG = 0;
-const int PANELS_INV   = 1;
-const int SIZE_PANELS  = 2;
+const int PANELS_DEBUG      = 0;
+const int PANELS_INV        = 1;
+const int PANELS_TIME_CHART = 2;
+const int SIZE_PANELS       = 3;
 
-/* Unicode Blocks
-U+2581	▁	Lower one eighth block
-U+2582	▂	Lower one quarter block
-U+2583	▃	Lower three eighths block
-U+2584	▄	Lower half block
-U+2585	▅	Lower five eighths block
-U+2586	▆	Lower three quarters block
-U+2587	▇	Lower seven eighths block
-U+2588	█	Full block
-
-U+2588	█	Full block
-U+2589	▉	Left seven eighths block
-U+258A	▊	Left three quarters block
-U+258B	▋	Left five eighths block
-U+258C	▌	Left half block
-U+258D	▍	Left three eighths block
-U+258E	▎	Left one quarter block
-U+258F	▏	Left one eighth block
-
-U+2591	░	Light shade
- */
-
-List<string> _panel_tags = new List<string>(SIZE_PANELS) { "@DebugDisplay", "@InventoryDisplay" };
+List<string> _panel_tags = new List<string>(SIZE_PANELS) { "@DebugDisplay", "@InventoryDisplay", "@TimeChartDisplay" };
 
 /* Genuine global state */
 List<int> _cycles = new List<int>(SIZE_CYCLES);
 
 List<List<IMyTextPanel>> _panels = new List<List<IMyTextPanel>>(SIZE_PANELS);
-List<string> _panel_text = new List<string>(SIZE_PANELS) { "", "" };
+List<string> _panel_text = new List<string>(SIZE_PANELS) { "", "", "" };
 
 string _inv_text = "", _cargo_text = ""; // FIXME: StringBuilder?
 
@@ -61,6 +40,10 @@ List<IMyCargoContainer> _cargo_blocks = new List<IMyCargoContainer>();
 List<long> _load = new List<long>();
 List<long> _time = new List<long>();
 List<Dictionary<string, MyFixedPoint>> _item_counts = new List<Dictionary<string, MyFixedPoint>>(INV_HISTORY);
+
+// 42x28 seems about right for 1x1 panel at 0.6
+DrawBuffer _chart_buffer = new DrawBuffer(42, 28);
+Chart _time_chart = new Chart(true, "Exec Time", "us");
 
 class CargoSample {
     public MyFixedPoint UsedMass, UsedVolume, MaxVolume;
@@ -99,6 +82,8 @@ public Program() {
     FindPanels();
     FindInventoryBlocks();
     FindCargoBlocks();
+
+    SetupTimeChart();
 
     if (!Me.CustomName.Contains(_script_name)) {
         // Update our block to include our script name
@@ -149,6 +134,9 @@ public void Main(string argument, UpdateType updateSource) {
             CompositeInventoryPanel();
 
             FlushToPanels(PANELS_INV);
+
+            UpdateTimeChart();
+            FlushToPanels(PANELS_TIME_CHART);
 
 	    _load[LoadOffset(0)] = Runtime.CurrentInstructionCount;
 	    _time[TimeOffset(0)] = (DateTime.Now - start_time).Ticks;
@@ -369,4 +357,207 @@ public void FlushToPanels(int kind) {
 public void Log(string s) {
     WritePanels(PANELS_DEBUG, s + "\n");
     Echo(s);
+}
+
+public void SetupTimeChart() {
+    _time_chart.AddBuffer(_chart_buffer, 0, 0, 42, 12);
+    _time_chart.AddBuffer(_chart_buffer, 0, 12, 21, 12);
+}
+
+public void UpdateTimeChart() {
+    ClearPanels(PANELS_TIME_CHART);
+
+    _chart_buffer.Reset();
+    int max = (int)_time.Max();
+    // Start at -1 because "now" hasn't been updated yet.
+    for (int i = 0; i < TIME_HISTORY - 1; i++) {
+        //Log($"Update T-{i,-2}");
+        _time_chart.DrawBar(i, (int)_time[TimeOffset(-i - 1)], max);
+    }
+
+    WritePanels(PANELS_TIME_CHART, _chart_buffer.ToString());
+}
+
+public class DrawBuffer {
+    public int X, Y;
+    public List<StringBuilder> Buffer;
+    private List<string> template;
+    string blank;
+
+    public DrawBuffer(int x, int y) {
+        X = x;
+        Y = y;
+        blank = new String(' ', X) + "\n";
+        Buffer = new List<StringBuilder>(Y);
+        template = new List<string>(Y);
+        for (int i = 0; i < Y; i++) {
+            Buffer.Add(new StringBuilder(blank));
+            template.Add(blank);
+        }
+    }
+
+    public void Save() {
+        for (int i = 0; i < Y; i++) {
+            template[i] = Buffer[i].ToString();
+        }
+    }
+
+    public void Reset() {
+        for (int i = 0; i < Y; i++) {
+            Buffer[i].Clear().Append(template[i]);
+        }
+    }
+
+    public void Clear() {
+        for (int i = 0; i < Y; i++) {
+            Buffer[i].Clear().Append(blank);
+        }
+    }
+
+    public void Write(int x, int y, string s) {
+        if (s.Length == 1) {
+            Buffer[y][x] = s[0];
+        } else {
+            Buffer[y].Remove(x, s.Length).Insert(x, s);
+        }
+    }
+
+    override public string ToString() {
+        return string.Concat(Buffer);
+    }
+}
+
+public class ViewPort {
+    private DrawBuffer buffer;
+    private int offsetX, offsetY;
+    public int X, Y;
+
+    public ViewPort(DrawBuffer buff, int offset_x, int offset_y, int x, int y) {
+        buffer = buff;
+        offsetX = offset_x;
+        offsetY = offset_y;
+        X = x;
+        Y = y;
+    }
+
+    public void Save() {
+        buffer.Save();
+    }
+
+    public void Write(int x, int y, string s) {
+        // Yeah, no bounds checking. Sue me.
+        buffer.Write(offsetX + x, offsetY + y, s);
+    }
+}
+
+public class Chart {
+    /* Unicode Blocks
+    U+2581	▁	Lower one eighth block
+    U+2582	▂	Lower one quarter block
+    U+2583	▃	Lower three eighths block
+    U+2584	▄	Lower half block
+    U+2585	▅	Lower five eighths block
+    U+2586	▆	Lower three quarters block
+    U+2587	▇	Lower seven eighths block
+    U+2588	█	Full block
+
+    U+2588	█	Full block
+    U+2589	▉	Left seven eighths block
+    U+258A	▊	Left three quarters block
+    U+258B	▋	Left five eighths block
+    U+258C	▌	Left half block
+    U+258D	▍	Left three eighths block
+    U+258E	▎	Left one quarter block
+    U+258F	▏	Left one eighth block
+
+    U+2591	░	Light shade
+     */
+    static List<string> _y_blocks = new List<string>(8) {
+      " ", "\u2581", "\u2582", "\u2583", "\u2584", "\u2585", "\u2586", "\u2587", "\u2588",
+    };
+    static List<string> _x_blocks = new List<string>(8) {
+      " ", "\u258F", "\u258E", "\u258D", "\u258C", "\u258B", "\u258A", "\u2589", "\u2588",
+    };
+
+    private List<ViewPort> viewports;
+    public bool Horizontal;
+    public string Title;
+    public string Unit;
+
+    public Chart(bool horizontal, string title, string unit) {
+        viewports = new List<ViewPort>();
+        Horizontal = horizontal;
+        Title = title;
+        Unit = unit;
+    }
+
+    public void AddViewPort(ViewPort viewport) {
+        viewports.Add(viewport);
+
+	viewport.Write(0, 0, "." + new String('-', viewport.X - 2) + ".");
+	for (int i = 1; i < viewport.Y - 1; i++) {
+	    viewport.Write(0, i, "|");
+	    viewport.Write(viewport.X - 1, i, "|");
+	}
+	viewport.Write(0, viewport.Y - 1, "." + new String('-', viewport.X - 2) + ".");
+	viewport.Save();
+    }
+
+    public void AddBuffer(DrawBuffer buffer, int offset_x, int offset_y, int x, int y) {
+        AddViewPort(new ViewPort(buffer, offset_x, offset_y, x, y));
+    }
+
+    private void DrawBarToViewPort(ViewPort viewport, int t, int val, int max) {
+	int x, y, size, dx, dy;
+	List<string> blocks;
+
+        if (Horizontal) {
+	    x = viewport.X - 2 - t;
+            if (x < 1)
+                return;
+            y = viewport.Y - 2;
+	    size = viewport.Y - 2;
+	    blocks = _y_blocks;
+	    dx = 0;
+            dy = -1;
+        } else {
+	    x = 1;
+            y = viewport.Y - 2 - t;
+            if (y < 1)
+                return;
+	    size = viewport.X - 2;
+	    blocks = _x_blocks;
+	    dx = 1;
+            dy = 0;
+        }
+
+        //parent.Log($"DrawBarToVP t{t}, v{val}, m{max}\nx{x}, y{y}, s{size}, dx{dx}, dy{dy}");
+
+	int scale = max / size;
+	if (scale < 1)
+	    scale = 1;
+	int repeat = val / scale;
+	int fraction = ((val % scale) * 8) / scale;
+
+	fraction = fraction >= 4 ? 4 : 0; // Only half block is implemented in font.
+
+	for (int i = 0; i < size; i++) {
+	    if (i < repeat) {
+		//buffer.Buffer[y][x] = blocks[8][0];
+		viewport.Write(x, y, blocks[8]);
+	    } else if (i == repeat) {
+		//buffer.Buffer[y][x] = blocks[fraction][0];
+		viewport.Write(x, y, blocks[fraction]);
+		break;
+	    }
+	    x += dx;
+	    y += dy;
+	}
+    }
+
+    public void DrawBar(int t, int val, int max) {
+        for (int i = 0, sz = viewports.Count; i < sz; i++) {
+            DrawBarToViewPort(viewports[i], t, val, max);
+        }
+    }
 }
